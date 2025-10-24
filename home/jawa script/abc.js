402eba7ab397077dfdf24da52d3556bab19310f3
Lakshya <partaplakshay03@gmail.com>
2025-10-24 10:46:05 +0530

    add my html file

---
+++ home/jawa script/abc.js
@@ -0,0 +1,9 @@
+for(let i=1;i<6;i++){
+  let temp=""
+
+  for(let j=1;j<6;j++){
+    temp+="*"
+  }
+  console.log(temp);
+
+}
---
+++ home/jawa script/b.js
@@ -0,0 +1,49 @@
+for(let i=6;i>=1;i--){
+    let temp =""
+    for(let j=i;j>=1;j--){
+        temp+=("A")
+    }
+    console.log(temp);
+}
+
+
+for(let i=6;i>=1;i--){
+    let temp =""
+    for(let j=i;j>=1;j--){
+        temp+=(i,j)
+    }
+    console.log(temp);
+}
+
+for(let i=0;i<4;i++){
+    let temp =" "
+    for(let j=0;j<=i;j++){
+        temp+=i
+    }
+    console.log(temp);
+}
+
+
+for(let i=0;i<4;i++){
+    let temp =" "
+    for(let j=i;j<=4;j++){
+        temp+=i
+    }
+    console.log(temp);
+}
+
+
+for(let i=0;i<4;i++){
+    let temp =' '
+    for(let j=0;j<=i;j++){
+        temp+='*'
+    }
+    console.log(temp);
+}
+for(let i=0;i<4;i++){
+    let temp =' '
+    for(let j=i;j<=4;j++){
+        temp+='*'
+    }
+    console.log(temp);
+}
